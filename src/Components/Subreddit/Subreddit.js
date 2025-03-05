2676ee1589f2d73a90700393aacda0d5b3486bcc
Micah Steele <micahs66@gmail.com>
2025-03-06 09:31:46 +1100

    subreddit button hover and focus

--- src/Components/Subreddit/Subreddit.js
+++ src/Components/Subreddit/Subreddit.js
@@ -3,8 +3,10 @@ import './Subreddit.css'
 const Subreddit = ({name, img, index}) => {
     return (
         <ul className='subreddit'>
-            {<img src={img}/>}
-            <li>{name}</li>
+            <button className='subreddit-button'>
+                {<img src={img}/>}
+                <li className='subreddit-name'>{name}</li>
+            </button>
         </ul>
     )
 };
